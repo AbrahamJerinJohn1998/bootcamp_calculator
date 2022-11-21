16ac8d1285bf429c2807b7c4084414a1fe0c5d41
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-21 11:06:54 +0530

    Main

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        int a,b,c,k;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number:");
+        a=sc.nextInt();
+        System.out.println("Enter second number:");
+        b=sc.nextInt();
+        System.out.println("enter 1-Addition 2-Subtraction 3-Multiplication 4-Division : ");
+        c=sc.nextInt();
+
+    }
+}
